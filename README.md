746ae067731e8f7e6a5d52cfc9498eb3c1d7e736
John Gedeon <js1@gedeons.com>
2020-12-05 01:52:47 -0800

    Add doit file and consolidate readme

--- README.md
+++ README.md
@@ -3,6 +3,27 @@ My solutions to the advent of code
 
 ## 2020
 
-### Day 1 Repair Report
+### Build
 
-* [README.md](2020/day1_report_repair)
+`docker-compose build`
+
+### Running tests
+
+`docker-compose run --rm 2020day5 pytest`
+
+### Running code
+
+`docker-compose run --rm 2020day5`
+
+### other
+
+https://pydoit.org/
+https://docker-sync.readthedocs.io/en/latest/
+
+`doit list`
+
+`doit test`
+`doit test:day1`
+
+`doit exec`
+`doit exec:day1`
---
+++ dodo.py
@@ -0,0 +1,27 @@
+from doit.action import CmdAction
+
+
+def task_sync():
+    """start docker-sync"""
+
+    return {
+        'actions': ['docker-sync start'],
+        }
+
+
+def task_test():
+    """run tests for day"""
+
+    for day in [1, 2, 3, 4, 5]:
+        yield {'name': f'day{day}',
+               'verbosity': 2,
+               'actions': [f'docker-compose -f docker-compose.yml -f docker-compose-dev.yml run --rm 2020day{day} pytest']}
+
+
+def task_test():
+    """run tests for day"""
+
+    for day in [1, 2, 3, 4, 5]:
+        yield {'name': f'day{day}',
+               'verbosity': 2,
+               'actions': [f'docker-compose -f docker-compose.yml -f docker-compose-dev.yml run --rm 2020day{day} pytest']}
